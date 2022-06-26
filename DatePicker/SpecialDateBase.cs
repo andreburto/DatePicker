0b1749f54250613d007b5fec19898ce539164119
Andrew Burton <tuglyraisin@gmail.com>
2022-06-26 15:25:39 -0500

    Adding files.

---
+++ DatePicker/SpecialDateBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace DatePicker
+{
+    class SpecialDateBase
+    {
+        protected String _getKeyFromDate(int m, int d)
+        {
+            return String.Format("{0}{1}", _prependZero(m), _prependZero(d));
+        }
+
+        protected String _prependZero(int n)
+        {
+            string TwoDigits = n.ToString();
+            if (n < 10)
+            {
+                TwoDigits = String.Format("0{0}", TwoDigits);
+            }
+            return TwoDigits;
+        }
+    }
+}
